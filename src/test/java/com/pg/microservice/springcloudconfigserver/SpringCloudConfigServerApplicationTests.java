eda6c84187fcd65765ae5e754dc57cea4a837d5b
Praroop Gupta <praroop.gupta193@gmail.com>
2020-05-07 09:04:00 +0530

    Initial Commit

---
+++ src/test/java/com/pg/microservice/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pg.microservice.springcloudconfigserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
